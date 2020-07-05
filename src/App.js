7cd035bf9efc7575e41c3b5871bd05a842de4645
tiana30 <tiana3059@gmail.com>
2020-07-05 10:52:37 +0700

    add state and filteredCourses

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, {useState} from 'react';
+import CoursesList from './coursesList';
 
 const courses = [
   {
@@ -32,25 +33,30 @@ const courses = [
 ];
 
 const App = () => {
+  const [searchText, setSearchText] = useState('');
+
+  const handleSearchInputChange = event => {
+    setSearchText(event.target.value);
+  }
+
+  const filteredCourses = courses.filter(course => {
+    return course.title.includes(searchText) || course.author.includes(searchText);
+  });
 
     return (
       <div>
       <h1>List of Courses</h1>
       <hr />
-      {courses.map(function(course) {
-        return (
-          <div key={course.id}>
-            <span>
-              <a href={course.url}><h4>{course.title}</h4></a>
-            </span>
-            <span>by <strong>{course.author}</strong></span>
-            <span> | Video Hours: {course.hours_video}</span>
-            <span> | Number of Lectures: {course.number_of_lectures}</span>
-            <span> | Rating: {course.rating}</span>
-            <br/><br/>
-          </div>
-        );
-          })} 
+
+      <label htmlFor = "searchInput"> Search : </label>
+      <input 
+      id="searchInput" 
+      type="text" 
+      onChange = {handleSearchInputChange}
+      
+      />
+      
+      <CoursesList courses={filteredCourses} />
         </div>
       
     ); 
