d892d34618f5256de5206b249930c14109d591cf
tiana30 <tiana3059@gmail.com>
2020-07-05 07:52:44 +0700

    add component course

--- src/App.js
+++ src/App.js
@@ -1,26 +1,58 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+const courses = [
+  {
+    id:1,
+    title: "React - The Complete Guide (incl Hooks, React Router, Redux)",
+    author: "Maximilian Schwarzmülller",
+    hours_video: 40.5,
+    number_of_lectures: 490,
+    rating: 4.6,
+    url: "https://codingthesmartway.com/courses/react-complete-guide/"
+  },
+  {
+    
+    id:2,
+    title: "Modern React with Redux",
+    author: "Stephen Grider",
+    hours_video: 47.5,
+    number_of_lectures: 488,
+    rating: 4.6,
+    url: "https://codingthesmartway.com/courses/modern-react-with-redux/"
+  },
+  {
+    id:3,
+    title: "The Complete React Developer Course (w/ Hooks and Redux)",
+    author: "Andrew Mead",
+    hours_video: 39,
+    number_of_lectures: 200,
+    rating: 4.7,
+    url: "http://codingthesmartway.net/courses/complete-react-web-app-developer/"
+  }
+];
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
+    return (
+      <div>
+      <h1>List of Courses</h1>
+      <hr />
+      {courses.map(function(course) {
+        return (
+          <div key={course.id}>
+            <span>
+              <a href={course.url}><h4>{course.title}</h4></a>
+            </span>
+            <span>by <strong>{course.author}</strong></span>
+            <span> | Video Hours: {course.hours_video}</span>
+            <span> | Number of Lectures: {course.number_of_lectures}</span>
+            <span> | Rating: {course.rating}</span>
+            <br/><br/>
+          </div>
+        );
+          })} 
+        </div>
+      
+    ); 
+}
 export default App;
