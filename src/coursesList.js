ac8ff8c9be243c5a95f25c4d3f5c79908fcbaaa4
tiana30 <tiana3059@gmail.com>
2020-07-05 08:28:18 +0700

    add CoursesList

---
+++ src/coursesList.js
@@ -0,0 +1,19 @@
+import React from 'react';
+function CoursesList(props){
+    return props.courses.map(function(course){
+        return(
+        <div key={course.id}>
+            <span>
+              <a href={course.url}><h4>{course.title}</h4></a>
+            </span>
+            <span>by <strong>{course.author}</strong></span>
+            <span> | Video Hours: {course.hours_video}</span>
+            <span> | Number of Lectures: {course.number_of_lectures}</span>
+            <span> | Rating: {course.rating}</span>
+            <br/><br/>
+          </div>
+        );
+  });     
+}
+
+export default CoursesList;
